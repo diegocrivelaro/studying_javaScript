39c920c64c256ae58d581d284432b35705c4401f
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2021-12-22 16:09:29 -0300

    :white_check_mark: Add destruct functions

---
+++ Intermediate_JavaScript/destructuring_assignment/Functions/javascript.js
@@ -0,0 +1,32 @@
+const client = {
+  name: 'Diego',
+  cpf: '123.456.789-10',
+  telephone: '(11) 99999-9999',
+  address: {
+    city: 'Balneário Camboriú',
+    state: 'SC',
+  },
+};
+
+/* Standard */
+function y() {
+  console.log(
+    'Standard: ',
+    client.name,
+    client.cpf,
+    client.address.city
+  );
+}
+
+/* Destructuring */
+function x({ name, cpf, address: { city } }) {
+  console.log(
+    'Destructuring: ',
+    name,
+    cpf,
+    city
+  );
+}
+
+y(client);
+x(client);
