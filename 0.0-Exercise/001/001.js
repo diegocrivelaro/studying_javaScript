f47e861a9755ea4969a681fda66a018a37d38fbf
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-01-27 16:46:29 -0300

    ✅ Add exercise 001

---
+++ 0.0-Exercise/001/001.js
@@ -0,0 +1,8 @@
+/*
+Create a script that reads a person's name and displays a welcome message according to the value entered
+*/
+
+let name = prompt('What is your name?');
+
+console.log(`Hello ${name}!`);
+window.alert(`Hello ${name}!`);
