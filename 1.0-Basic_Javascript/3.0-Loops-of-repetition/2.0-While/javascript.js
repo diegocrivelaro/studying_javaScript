4cd8fe654c77f8953641b499268fdfbc3ebad13a
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-01-27 16:49:04 -0300

    ✅ Add repeat loop studies

---
+++ 1.0-Basic_Javascript/3.0-Loops-of-repetition/2.0-While/javascript.js
@@ -0,0 +1,16 @@
+let x = 0;
+
+while (x <= 50) {
+  console.log(x);
+  x++;
+}
+
+/*---------------*/
+
+let array = ['Home', 'Pen', 'Apple'];
+let i = 0;
+
+while (i < array.length) {
+  console.log(array[i]);
+  i++;
+}
---
+++ 1.0-Basic_Javascript/3.0-Loops-of-repetition/3.0-Do_While/javascript.js
@@ -0,0 +1,6 @@
+let i = 1;
+
+do {
+  console.log("Digitando..."+i)
+  i++
+} while(i <= 10)
