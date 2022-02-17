2d6608b79137d7e100323bba314918d103ca4133
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-02-17 07:43:50 -0300

    ✅ Add switch conditional structure

---
+++ 1.0-Basic_Javascript/2.0-Conditional_structures/2.0-Switch/javascript.js
@@ -0,0 +1,14 @@
+var month = 2;
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  default:
+    console.log("Month is not january, february or march");
+}
